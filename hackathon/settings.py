e2e3b9da78632b9ba0d277518776acd421cba143
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-04-27 00:58:22 +0530

    no static

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+r9ykd9e2+fjms!z_r82jl^v$=0*vbu=&*cx&3$h1bx_h0chof'
+SECRET_KEY = 'django-hello-+r9ykd9e2+fjms!z_r82jl^v$=0*vbu=&*cx&3$h1bx_h0chof'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -128,6 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # AUTH_USER_MODEL = 'mainapp.User'
 
